45b9574e664509daa4cc71c78a4841bf57115237
seobkeom <ajm153123@naver.com>
2023-09-06 21:19:44 +0900

    BOJ 15989(DP)

---
+++ Baekjoon/src/BOJ/BOJ_15989.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_15989 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[][] dp = new int[10001][4];
+		dp[1][1] = 1;
+		dp[1][2] = 0;
+		dp[1][3] = 0;
+
+		dp[2][1] = 1;
+		dp[2][2] = 1;
+		dp[2][3] = 0;
+
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		dp[3][3] = 1;
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while(t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			for(int i = 4;i<=n;i++) {
+				dp[i][1] = dp[i-1][1];
+				dp[i][2] = dp[i-2][1]+dp[i-2][2];
+				dp[i][3] = dp[i-3][1]+dp[i-3][2]+dp[i-3][3];
+			}
+			sb.append(dp[n][1]+dp[n][2]+dp[n][3]+"\n");
+		}
+		System.out.println(sb);
+	}
+}
