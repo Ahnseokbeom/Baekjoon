9e9684211e998550e593924520dee7d9a8835742
seobkeom <ajm153123@naver.com>
2023-09-07 16:14:24 +0900

    BOJ 2606 Code Update(Insert DFS)

--- Baekjoon/src/BOJ/BOJ_2606.java
+++ Baekjoon/src/BOJ/BOJ_2606.java
@@ -1,46 +1,55 @@
 package BOJ;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ_2606 {
-	static boolean[] visit;
-	static int[][] check;
-	static int n;
+	static boolean[] check;
+	static int[][] arr;
+	static int n,m;
+	static int count = 0;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		int m = sc.nextInt();
-		check = new int[n+1][n+1];
-		visit = new boolean[n+1];
-		Arrays.fill(visit, false);
-		for(int i = 0;i<m;i++) {
+		m = sc.nextInt();
+		arr = new int[n+1][n+1];
+		check = new boolean[n+1];
+		for(int i=1;i<=m;i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			check[x][y] = 1;
-			check[y][x] = 1;
+			arr[x][y] = 1;
+			arr[y][x] = 1;
 		}
+
+		System.out.println(bfs(1));
+		System.out.println(dfs(1));
 		sc.close();
-		bfs(1);
 	}
-	public static void bfs(int v) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		visit[v] = true;
-		q.offer(v);
-		int count = 0;
+	public static int bfs(int x) {
+		Queue<Integer> q = new LinkedList<>();
+		check[x] = true;
+		q.offer(x);
 		while(!q.isEmpty()) {
-			int x = q.poll();
-			for(int i = 1;i<check.length;i++) {
-				if(check[x][i] == 1 && visit[i] == false) {
+			int num = q.poll();
+			for(int i = 1;i<arr.length;i++) {
+				if(arr[num][i]==1 && !check[i]) {
 					q.offer(i);
-					visit[i] = true;
+					check[i] = true;
 					count++;
 				}
 			}
 		}
-		System.out.println(count);
+		return count;
 	}
-
+	public static int dfs(int x) {
+		check[x] = true;
+		for(int i = 1;i<=n;i++) {
+			if(arr[x][i]==1 && !check[i]) {
+				count++;
+				dfs(i);
+			}
+		}
+		return count;
+	}
 }
