3e74095625225a721464451a3f1f3f4414584eae
seobkeom <ajm153123@naver.com>
2023-09-10 20:27:46 +0900

    BOJ_1743 Code Update(DFS)

--- Baekjoon/src/BOJ/BOJ_1743.java
+++ Baekjoon/src/BOJ/BOJ_1743.java
@@ -6,46 +6,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_1743 {
+	static int n,m,k,num;
 	static int[][] arr;
-	static int[] dx = {-1,0,1,0};
-	static int[] dy = {0,1,0,-1};
-	static boolean[] visit;
-	static int n,m;
-	static int temp, answer;
+	static boolean[][] check;
+	static int[] dx = {0,0,1,-1};
+	static int[] dy = {-1,1,0,0};
+	static int max = 0;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
 		arr = new int[n+1][m+1];
+		check = new boolean[n+1][m+1];
 		for(int i = 0;i<k;i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			arr[r][c] = 1;
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[x][y] = 1;
 		}
 		for(int i = 1;i<=n;i++) {
 			for(int j = 1;j<=m;j++) {
-				if(arr[i][j]==1) {
-					temp = 0;
+				if(arr[i][j]==1 && !check[i][j]) {
+					num = 0;
 					dfs(i,j);
-					answer = Math.max(answer, temp);
+					max = Math.max(max,num);
 				}
 			}
 		}
-		System.out.println(answer);
+		System.out.println(max);
 	}
-	private static void dfs(int x, int y) {
-		arr[x][y] = 0;
-		temp++;
+	public static void dfs(int x, int y) {
+		check[x][y] = true;
+		num++;
 		for(int i = 0;i<4;i++) {
-			int nowX = x+dx[i];
-			int nowY = y+dy[i];
-			if((1<=nowX&&nowX<n+1) && (1<=nowY&&nowY<m+1) && (arr[nowX][nowY]==1)){
-				dfs(nowX,nowY);
+			int nx = x+dx[i];
+			int ny = y+dy[i];
+			if(nx > 0 && nx <= n && ny > 0 && ny <= m) {
+				if(arr[nx][ny]==1 && !check[nx][ny]) {
+					check[nx][ny] = true;
+					dfs(nx,ny);
+				}
 			}
 		}
 	}
-
 }
