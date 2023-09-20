abb4f50aaf8c74d18d6c697a37472d29995a8d1e
seobkeom <ajm153123@naver.com>
2023-09-20 18:31:26 +0900

    BOJ 2133(DP)

--- Baekjoon/src/BOJ/BOJ_2133.java
+++ Baekjoon/src/BOJ/BOJ_2133.java
@@ -3,12 +3,22 @@ package BOJ;
 import java.util.Scanner;
 
 public class BOJ_2133 {
+	static int[] dp;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		if(n%3==0 || n==1) System.out.println(0);
-		else System.out.println(3*(n/2));
+		dp = new int[n+1];
+		System.out.println(DP(n));
 		sc.close();
 	}
-
+	public static int DP(int n) {
+		dp[0] = 1;
+		for(int i = 2;i<=n;i+=2) {
+			dp[i] = dp[i-2]*3;
+			for(int j = i-4;j>=0;j-=2) {
+				dp[i]+=dp[j]*2;
+			}
+		}
+		return n%2==0?dp[n]:0;
+	}
 }
