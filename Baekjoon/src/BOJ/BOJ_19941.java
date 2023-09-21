35fb95142e341d60bf81223adcbc86b4c29c11c6
seobkeom <ajm153123@naver.com>
2023-09-21 16:46:20 +0900

    BOJ 2Problems

---
+++ Baekjoon/src/BOJ/BOJ_19941.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_19941 {
+	static int n,k;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		String s = br.readLine();
+		String[] str = s.split("");
+		boolean[] check = new boolean[n];
+		int answer = 0;
+		for(int i = 0;i<n;i++) {
+			if(str[i].equals("P")) {
+				int left = Math.max(i-k, 0);
+				int right = Math.min(i+k, n-1);
+				for(int j = left;j<=right;j++) {
+					if(str[j].equals("H") && !check[j]) {
+						check[j] = true;
+						answer++;
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
