3909c42bf2f7bc6558806139e390ae18b6289d5a
seobkeom <ajm153123@naver.com>
2023-09-21 16:01:40 +0900

    BOJ 2210 Code Update(List To Set)

--- Baekjoon/src/BOJ/BOJ_2210.java
+++ Baekjoon/src/BOJ/BOJ_2210.java
@@ -4,16 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_2210 {
 	static List<String> list = new ArrayList<>();
+	static HashSet<String> set = new HashSet<>();
 	static int[] dx = {-1,1,0,0};
 	static int[] dy = {0,0,1,-1};
+	static String[][] map;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[][] map = new String[5][5];
+		map = new String[5][5];
 		for(int i = 0;i<5;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine()," ");
 			for(int j = 0;j<5;j++) {
@@ -22,25 +25,22 @@ public class BOJ_2210 {
 		}
 		for(int i = 0;i<5;i++) {
 			for(int j = 0;j<5;j++) {
-				dfs(map,i,j,0,map[i][j]);
+				dfs(i,j,0,map[i][j]);
 			}
 		}
-		System.out.println(list.size());
-
+		System.out.println(set.size());
 
 	}
-	private static void dfs(String[][] map, int x, int y, int count, String temp) {
+	private static void dfs(int x, int y, int count, String s) {
 		if(count==5) {
-			if(!list.contains(temp)) {
-				list.add(temp);
-			}
+			set.add(s);
 			return;
 		}
 		for(int i = 0;i<4;i++) {
 			int nowX = x+dx[i];
 			int nowY = y+dy[i];
 			if((0<=nowX&&nowX<5) && (0<=nowY && nowY < 5)) {
-				dfs(map,nowX,nowY,count+1,temp+map[nowX][nowY]);
+				dfs(nowX,nowY,count+1,s+map[nowX][nowY]);
 			}
 		}
 	}
