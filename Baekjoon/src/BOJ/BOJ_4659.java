cf5f453956e74e36198fb892202c95192cb367aa
seobkeom <ajm153123@naver.com>
2023-09-22 17:50:42 +0900

    BOJ 구현 2Problems

---
+++ Baekjoon/src/BOJ/BOJ_4659.java
@@ -0,0 +1,25 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BOJ_4659 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String answer = "";
+		Pattern pattern = Pattern.compile("^[^aeiou]+$|[aeiou]{3,}|[^aeiou]{3,}|([^eo])\\1+");
+		Matcher matcher;
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			String s = br.readLine();
+			if(s.equals("end")) break;
+			matcher = pattern.matcher(s);
+			answer = matcher.find()?"not acceptable.":"acceptable.";
+			sb.append("<"+s+"> is "+ answer+"\n");
+		}
+		System.out.println(sb);
+	}
+}
