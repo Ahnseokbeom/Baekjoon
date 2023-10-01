bfd05ba50cc99ff74d7fe8e06e22aeda5d04d4d8
seobkeom <ajm153123@naver.com>
2023-10-01 20:18:35 +0900

    BOJ 12738 Code Update

--- Baekjoon/src/BOJ/BOJ_12738.java
+++ Baekjoon/src/BOJ/BOJ_12738.java
@@ -3,37 +3,37 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ_12738 {
+	static int[] list;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n+1];
-		ArrayList<Integer> list = new ArrayList<>();
-		list.add(Integer.MIN_VALUE);
+		int[] arr = new int[n];
 
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		for(int i = 1;i<=n;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		for(int i = 1;i<=n;i++) {
-			if(list.get(list.size()-1)<arr[i]) list.add(arr[i]);
-			else {
-				int left = 1;
-				int right = list.size()-1;
-				while(left < right) {
-					int mid = (left+right)/2;
-					if(list.get(mid) <= arr[i]) {
-						left = mid+1;
-					}else {
-						right = mid;
-					}
-				}
-				list.set(right, arr[i]);
+		for(int i = 0;i<n;i++) arr[i] = Integer.parseInt(st.nextToken());
+		list = new int[n+1];
+		list[0] = -100000001;
+		int answer = 0;
+		int idx = 0;
+		for(int i = 0;i<n;i++) {
+			if(arr[i] > list[answer]) list[++answer] = arr[i];
+			else{
+				idx = BinarySearch(0,answer,arr[i]);
+				list[idx] = arr[i];
 			}
 		}
-		System.out.println(list.size()-1);
+		System.out.println(answer);
+	}
+	public static int BinarySearch(int left, int right, int target) {
+		int mid = 0;
+		while(left < right) {
+			mid = (left+right)/2;
+			if(list[mid] < target) left = mid+1;
+			else right = mid;
+		}
+		return right;
 	}
 }
