9c571d768b97d26a58333e93b36a587edab67434
seobkeom <ajm153123@naver.com>
2023-12-05 11:51:20 +0900

    Feat : BOJ 17266 Code Update

--- Baekjoon/src/BOJ/Silver/BOJ_17266_BinarySearch.java
+++ Baekjoon/src/BOJ/Silver/BOJ_17266_BinarySearch.java
@@ -7,33 +7,46 @@ import java.util.StringTokenizer;
 
 public class BOJ_17266_BinarySearch {
 	static int n,m;
+	static int[] arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
+		arr = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0;i<m;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		System.out.println(binarySearch());
+	}
+	public static int binarySearch() {
 		int left = 1;
-		int right = n;
-		int answer = 0;
-		while(left <= right) {
-			int mid = (left+right)/2;
-			if(check(mid,arr)) {
-				answer = mid;
-				right = mid-1;
-			}else left = mid+1;
-		}
-		System.out.println(answer);
+        int right = n;
+        int answer = 0;
+
+        // 이분탐색
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (check(mid)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
 	}
-	public static boolean check(int num, int[] arr) {
-		int s = 0;
-		for(int i = 0;i<arr.length;i++) {
-			if(arr[i]-num<=s) s = arr[i]+num;
-			else return false;
-		}
-		return n-s<=0;
-	}
+	public static boolean check(int t) {
+        int s = 0;
+        for (int i = 0; i < m; i++) {
+            if (arr[i] - t <= s) {
+                s = arr[i] + t;
+            } else {
+                return false;
+            }
+        }
+        return n - s <= 0;
+    }
 }
