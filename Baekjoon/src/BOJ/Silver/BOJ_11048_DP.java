f192459937147fe6a7bfec08d24caadda23cb30d
seobkeom <ajm153123@naver.com>
2024-02-11 20:38:14 +0900

    BOJ_11048(DP)

---
+++ Baekjoon/src/BOJ/Silver/BOJ_11048_DP.java
@@ -0,0 +1,34 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11048_DP {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] candy = new int[n + 1][m + 1];
+
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= m; j++) {
+				candy[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int[][] dp = new int[n + 1][m + 1];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + candy[i][j];
+			}
+		}
+
+		System.out.println(dp[n][m]);
+	}
+}
